66fd86c15944a0267db470c338db690ccf47ab3a
Divy Srivastava <divysrivastava67@gmail.com>
2026-06-18 15:55:19 +0000

    operations done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,13 @@
+let v1 = "Hello"
+let v2 = " Shubh"
+let v3 = v1 + v2 // concatenation
+console.log(v3)
+
+// console.log( 3 + "hello")    3hello
+// console.log( 3 + 2 + "hii")  5hii
+// console.log("1" + 2 + 3)     123
+
+console.log(true) // prints true
+console.log(+true) // prints 1 which is kind of ok for "true" value but we see boolean to number conversion
+// console.log(true+) throws an error
+console.log(+"") // "" is false i.e. 0 therefore prints 0 string to number conversion
