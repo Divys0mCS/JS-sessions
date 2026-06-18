67ec52b36309cc00d4fbf41e099757c6682d4a83
Divy Srivastava <divysrivastava67@gmail.com>
2026-06-18 15:55:34 +0000

    operations done

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -7,6 +7,9 @@ console.log(v3)
 // console.log( 3 + 2 + "hii")  5hii
 // console.log("1" + 2 + 3)     123
 
+
+// these kind of conversions are not prefered in real world problems and production work
+
 console.log(true) // prints true
 console.log(+true) // prints 1 which is kind of ok for "true" value but we see boolean to number conversion
 // console.log(true+) throws an error
