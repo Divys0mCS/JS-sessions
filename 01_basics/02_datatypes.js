9aeff3ca86ebdc255ec6429cd8ff7d310e20b3d7
Divy Srivastava <divysrivastava67@gmail.com>
2026-06-18 10:14:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // it treats all JS code as newest version
+
+// Datatypes
+
+let name = "Shubh"
+let age = 17
+let isLoggedIn = true
+
+// 1. Number => 2 to power 53
+// 2. bigint
+// 3. string => ""
+// 4. boolean => true or false
+// 5. null => standalone value 
+// 6. Undefined => (value is not assigned rn)
+// 7. symbol => unique
+
+console.log(typeof "Shubh")  // typeof tells the type of any passed value ,identifier etc.
